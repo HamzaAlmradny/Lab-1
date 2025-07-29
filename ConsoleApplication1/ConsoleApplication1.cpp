e1e0ae5556f15031cf63b583be36608b47d6e6e1
Dr. Alsayed <Dr. Alsayed@DESKTOP-APIOASQ>
2025-07-29 20:44:58 +0300

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+
+   using namespace std;
+
+int main() {
+	int num1, num2, sum;
+	cout<< "Please, enter the first number:";
+	cin>> num1;
+	cout<< "Please, enter the second number:";
+	cin >> num2;
+	sum = num1 + num2;
+	cout << "The result is:" << sum;
+	
+	
+	
+	
+	
+	return 0;
+}
+
